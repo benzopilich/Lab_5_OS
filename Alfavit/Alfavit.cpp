e16b2db0c9da1f5ec0e9aa8059a2138952bec55b
Ilya <166957447+benzopilich@users.noreply.github.com>
2024-12-17 14:10:54 +0300

    Добавьте файлы проекта.

---
+++ Alfavit/Alfavit.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <cctype>
+using namespace std;
+
+void ErrorExit(const char* message) {
+    std::cerr << message << " Error: " << GetLastError() << std::endl;
+    exit(1);
+}
+
+int main() {
+    HANDLE hReadPipe = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hWritePipe = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Подключение к семафорам
+    HANDLE hServerSemaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"Semaphore");
+
+    if (!hServerSemaphore) {
+        ErrorExit("Failed to open semaphores.");
+    }
+
+    // Чтение данных из канала
+    WaitForSingleObject(hServerSemaphore, INFINITE);
+
+    int size, n;
+    DWORD read;
+    ReadFile(hReadPipe, &size, sizeof(size), &read, nullptr); // Читаем размер массива
+    ReadFile(hReadPipe, &n, sizeof(n), &read, nullptr);       // Читаем число N
+
+    std::vector<char> originalArray(size);     // Массив исходных символов
+    std::vector<char> processedArray(size);    // Массив обработанных символов
+
+    for (int i = 0; i < size; ++i) {
+        char ch;
+        ReadFile(hReadPipe, &ch, sizeof(ch), &read, nullptr);
+        originalArray[i] = ch;
+
+        // Проверяем, является ли символ цифрой или знаком препинания
+        if (isdigit(ch) || ispunct(ch)) {
+            processedArray[i] = ch;
+        }
+        else {
+            processedArray[i] = '-'; // Если не подходит, заменяем
+        }
+
+        WriteFile(hWritePipe, &processedArray[i], sizeof(char), &read, nullptr); // Отправляем обработанный символ
+    }
+
+
+    // Вывод массивов на консоль
+    std::cerr << "Original array: ";
+    for (char ch : originalArray) {
+        std::cerr << ch << " ";
+    }
+    std::cerr << std::endl;
+
+    std::cerr << "Processed array: ";
+    for (char ch : processedArray) {
+        std::cerr << ch << " ";
+    }
+    std::cerr << std::endl;
+    ReleaseSemaphore(hServerSemaphore, 1, nullptr);// уведомляем сервер
+    while(1){}
+    // Закрытие ресурсов
+    CloseHandle(hReadPipe);
+    CloseHandle(hWritePipe);
+    CloseHandle(hServerSemaphore);
+    return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+
+void ErrorExit(const char* message) {
+    std::cerr << message << " Error: " << GetLastError() << std::endl;
+    exit(1);
+}
+
+int main() {
+    HANDLE hReadPipe, hWritePipe;
+    SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), nullptr, TRUE };
+    sa.bInheritHandle = TRUE;
+   
+
+    // Создаем анонимный канал
+    if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0)) {
+        ErrorExit("Failed to create pipe.");
+    }
+
+    // Создаем семафор
+    HANDLE hServerSemaphore = CreateSemaphore(nullptr, 0, 1, L"Semaphore");
+
+    if (!hServerSemaphore) {
+        ErrorExit("Failed to create semaphores.");
+    }
+
+    // Подготовка запуска дочернего процесса
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi = {};
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(STARTUPINFO);
+    si.dwFlags |= STARTF_USESTDHANDLES;
+    si.hStdInput = hReadPipe;
+    si.hStdOutput = hWritePipe;
+
+    wchar_t cmdLine[] = L"Alfavit.exe";
+
+    if (!CreateProcess(nullptr, cmdLine, nullptr, nullptr, TRUE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &pi)) {
+        ErrorExit("Failed to create Alfavit process.");
+    }
+
+    // Ввод данных
+    std::cout << "Enter the size of the array: ";
+    int size;
+    std::cin >> size;
+
+    std::vector<char> array(size);
+    std::cout << "Enter array elements (char): ";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> array[i];
+    }
+
+    std::cout << "Enter a number N: ";
+    int n;
+    std::cin >> n;
+
+    // Передача данных в Alfavit
+    DWORD written;
+    WriteFile(hWritePipe, &size, sizeof(size), &written, nullptr); // Отправляем размер массива
+    WriteFile(hWritePipe, &n, sizeof(n), &written, nullptr);       // Отправляем число N
+
+    for (char ch : array) {
+        WriteFile(hWritePipe, &ch, sizeof(ch), &written, nullptr); // Отправляем элементы массива
+    }
+    ReleaseSemaphore(hServerSemaphore, 1, nullptr);           // Уведомляем alfavit
+
+    WaitForSingleObject(hServerSemaphore, INFINITE);
+
+    // Чтение обработанного массива
+    std::cout << "Processed characters received from Alfavit: ";
+    for (int i = 0; i < size; ++i) {
+        char processedChar;
+        DWORD read;
+        ReadFile(hReadPipe, &processedChar, sizeof(processedChar), &read, nullptr);
+        std::cout << processedChar << " ";
+    }
+
+    std::cout << std::endl;
+    // Закрытие ресурсов
+    CloseHandle(hReadPipe);
+    CloseHandle(hWritePipe);
+    CloseHandle(hServerSemaphore);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
+}
